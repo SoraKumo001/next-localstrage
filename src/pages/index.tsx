2e58e2fae9a23f30bfaf9edaae7726125d222842
SoraKumo <info@croud.jp>
2021-12-15 21:37:10 +0900

    first-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import {
+  setLocalStrage,
+  useGlobalState,
+} from "@react-libraries/use-global-state";
+
+setLocalStrage();
+
+const A = () => {
+  const [a, setA] = useGlobalState("a", "");
+  const [b, setB] = useGlobalState("b", "");
+  const [c, setC] = useState("");
+  return (
+    <div>
+      <div>
+        A(Global):
+        <input onChange={(e) => setA(e.currentTarget.value)} value={a} />
+      </div>
+      <div>
+        B(Global):
+        <input onChange={(e) => setB(e.currentTarget.value)} value={b} />
+      </div>
+      <div>
+        C(Local ):
+        <input onChange={(e) => setC(e.currentTarget.value)} value={c} />
+      </div>
+      <hr />
+    </div>
+  );
+};
+const Page = () => {
+  return (
+    <div>
+      <div>ウインドウ / コンポーネント間でデータを同期</div>
+      <hr />
+      <A />
+      <A />
+    </div>
+  );
+};
+
+export default Page;
